074de7ef4991ecaa2f57f1bc9b31917845316c88
xinran <wxinran6@vt.edu>
2023-02-27 23:48:11 +0000

    constructor component

--- Source/CryptRaider/TriggerComponents.cpp
+++ Source/CryptRaider/TriggerComponents.cpp
@@ -3,9 +3,29 @@
 
 #include "TriggerComponents.h"
 
+UTriggerComponents::UTriggerComponents()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// UE_LOG(LogTemp, Display, TEXT("Constructiong"));
+
+	// ...
+}
+
 void UTriggerComponents::BeginPlay()
 {
 	Super::BeginPlay();
 
 	UE_LOG(LogTemp, Display, TEXT("Trigger componenets alive"));
+}
+
+void UTriggerComponents::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// UE_LOG(LogTemp, Display, TEXT("Tick componenets alive"));
+
+
 }
--- Source/CryptRaider/TriggerComponents.h
+++ Source/CryptRaider/TriggerComponents.h
@@ -14,8 +14,13 @@ class CRYPTRAIDER_API UTriggerComponents : public UBoxComponent
 {
 	GENERATED_BODY()
 	
+public:
+	UTriggerComponents();	
 protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
-	
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 };
